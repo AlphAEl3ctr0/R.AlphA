4ca50037c9143270ffb1f0d6be8d83bc7be983ae
AlphAEl3ctr0 <raphael.flambard@outlook.fr>
2021-11-10 00:47:01 +0100

    correct and simplify computing in the timer function

--- R/timer.R
+++ R/timer.R
@@ -13,35 +13,47 @@
 #' @rawNamespace import(data.table, except =  c(month, hour, quarter, week, year, wday, second, minute, mday, yday, isoweek))
 #' @export
 timer <- function(timer_table = data.table(),start = FALSE, ...){
+	manualrun <- T
+	manualrun <- F
+	if (manualrun) {
+		library(lubridate)
+		start = 1
+		message("! parameters manually defined inside function for tests. Do not use results !")
+		timer_table = tm_Natmerge
+	}
 	# t1, t2
 		if(start) {
 			t1 <- Sys.time()
 		} else {
-			t1 <- as.POSIXct(as.POSIXlt(format(as.POSIXct(max(timer_table$heure))))) # voir si il existe une meilleure methode...
+			t1 <- max(timer_table$heure)
 		}
 		t2 <- Sys.time()
+		t1_s <- as.numeric(t1)
+		t2_s <- as.numeric(t2)
 	# dt : difference entre h_fin et h_debut	et remise en forme
-		dt_interval <- t1 %--% t2
-		dt_seconds <- dt_interval / dseconds() # pour recuperer une colonne numerique
-		dt_period <- as.period(dt_interval)
-		dt_second_part <- trunc(dt_period@.Data)
-		dt_ms_part <- trunc((dt_period@.Data - dt_second_part)*1000)
-		# dt <- sprintf('%02d:%02d,%03d', dt_period@minute, dt_second_part, dt_ms_part)
+		dt_seconds <- t2_s - t1_s
+		# message("dt_seconds :", round(dt_seconds, 3))
+		# dt_period <- as.period(dt_interval)
+		dt_ms_part     <- trunc(dt_seconds %% 1 * 1000)
+		dt_second_part <- trunc(dt_seconds / (1))          %% 60
+		dt_min_part    <- trunc(dt_seconds / (1*60))       %% 60
+		dt_hour_part   <- trunc(dt_seconds / (1*60*60))    %% 24
+		dt_day_part    <- trunc(dt_seconds / (1*60*60*24))
 		if (dt_seconds<60) {
 			# print("seconds only")
 			dt <- sprintf('%0d.%03d', dt_second_part, dt_ms_part)
 		} else if (dt_seconds<60*60){
 			# print("max : minutes")
-			dt <- sprintf('%0d:%02d.%03d', dt_period@minute, dt_second_part, dt_ms_part)
+			dt <- sprintf('%0d:%02d.%03d', dt_min_part, dt_second_part, dt_ms_part)
 		} else if (dt_seconds<60*60*24){
 			# print("max : hours")
-			dt <- sprintf('%0d:%02d:%02d.%03d',dt_period@hour, dt_period@minute, dt_second_part, dt_ms_part)
+			dt <- sprintf('%0d:%02d:%02d.%03d',dt_hour_part, dt_min_part, dt_second_part, dt_ms_part)
 		} else {
 			# print("max : more than hours anyway")
-			dt <- sprintf('%0d:%02d:%02d:%02d.%03d',dt_period@day, dt_period@hour, dt_period@minute, dt_second_part, dt_ms_part)
+			dt <- sprintf('%0d:%02d:%02d:%02d.%03d',dt_day_part, dt_hour_part, dt_min_part, dt_second_part, dt_ms_part)
 		}
 	# time_inter : remet les resultats sous forme de table
-		time_inter <- data.table(heure=t2, dt=dt, dt_seconds=dt_seconds)
+		time_inter <- data.table(heure=t2, dt=dt,heure_seconds = t2_s, dt_seconds=dt_seconds)
 	if (start) timer_table <- time_inter[0]
 	# ajout des eventuelles colonnes supplementaires
 		x = list(...)
