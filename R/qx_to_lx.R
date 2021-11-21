e3aaf906a91b09362c0519f231d3a89297df80c2
AlphAEl3ctr0 <raphael.flambard@outlook.fr>
2021-11-21 08:51:26 +0100

    Handle no-dim case in qx_to_lx

--- R/qx_to_lx.R
+++ R/qx_to_lx.R
@@ -8,16 +8,41 @@
 #' @export
 
 
-qx_to_lx <- function(data, dimsNames, incName, qxName = "qx"){
+qx_to_lx <- function(
+	data
+	, dimsNames = NULL
+	, incName
+	, qxName = "qx"
+){
+	manualrun <- T
+	manualrun <- F
+	if (manualrun) {
+		message("! parameters manually defined inside function for tests. Do not use results !")
+		data <- data.table(
+			inc_anc = 1:10
+			, res_rates = seq(from = 0.12, to = 0.03, length.out = 10)
+		)
+		incName = "inc_anc"
+		qxName = "res_rates"
+		dimsNames <- ""
+		rm(dimsNames)
+	}
 	melt_maintien <- copy(data)
 
 	melt_maintien[, retain_order := 1:.N]
 	melt_maintien[, px := 1-get(qxName)]
 	separator = "__"
-	melt_maintien[
-		, dims := do.call(paste, c(.SD, sep = separator))
-		, .SDcols = dimsNames
+	melt_maintien
+	if (missing(dimsNames)){
+		melt_maintien[
+			, dims := ""
 		]
+	} else {
+		melt_maintien[
+			, dims := do.call(paste, c(.SD, sep = separator))
+			, .SDcols = dimsNames
+			]
+	}
 	melt_maintien[, inc := get(incName)]
 	melt_maintien[
 		, key := do.call(paste, c(.SD, sep = separator))
@@ -31,7 +56,10 @@ qx_to_lx <- function(data, dimsNames, incName, qxName = "qx"){
 		, by = .(dims)
 		]
 	melt_maintien[, inc_p1 := inc+1]
-	melt_maintien[, key_xp1 := do.call(paste, c(.SD, sep = separator)), .SDcols = c("dims", "inc_p1")]
+	melt_maintien[
+		, key_xp1 := do.call(paste, c(.SD, sep = separator))
+		, .SDcols = c("dims", "inc_p1")
+		]
 
 	melt_maintien_lx <- merge(
 		melt_maintien
