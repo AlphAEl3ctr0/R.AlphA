33de839e81bd9b0ef54fb2737898c094656918bb
AlphAEl3ctr0 <raphael.flambard@outlook.fr>
2022-02-06 18:05:35 +0100

    dimsCol - correction, take names of input and not its copy - also, add perl option

--- R/dimsCol.R
+++ R/dimsCol.R
@@ -3,16 +3,27 @@
 #' @param data a data set in which columns are searched
 #' @param separator to separate column values
 #' @param pattern default is "dim_" to get dimensions. use another one if u like
+#' @param perl will be passed to grep when searching columns
 #' @importFrom data.table copy
 #' @export
-dimsCol <- function(data, separator = "_", pattern = "dim_"){
+dimsCol <- function(
+	data
+	, separator = "_"
+	, pattern = "dim_"
+	, perl = TRUE
+){
 	# voir si on a besoin d'importer data.table en entier ?
 	# data <- m_VIR # for manualRun
 
 	wkTbl <- as.data.table(copy(data))
 	wkTbl [, dimsCols_index := 1:.N]
 	{
-		dimColumns <- grep(names(wkTbl), pattern = pattern, value = T)
+		dimColumns <- grep(
+			names(data) # 2022.02.06 colonnes de data et non wkTbl, car on a rajoute des colonnes interm
+			, pattern = pattern
+			, value = T
+			, perl = perl
+		)
 		wkTbl[
 			, dims := do.call(paste, c(.SD, sep = separator))
 			, .SDcols = dimColumns
