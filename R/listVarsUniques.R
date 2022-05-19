8c6f83e1537ae3b075dbf9224004060172d6d0e7
AlphAEl3ctr0 <raphael.flambard@outlook.fr>
2022-05-19 13:54:25 +0200

    listVarsUniques - changing folder

--- R/listVarsUniques.R
+++ R/listVarsUniques.R
@@ -1,8 +1,16 @@
-listVarsUniques <- function(...){
+#' listing dimensions
+#' @description list unique values of columns for a set of tables
+#' @param ... should be a lsit a tables
+#' @export
+
+listVarsUniques <- function(..., pattern = ""){
 	manualrun <- T
 	manualrun <- F
 	if (manualrun) {
 		warning("! parameters manually defined inside function for tests. Do not use results !")
+		DT1 <- data.table(a = 1:10)
+		DT2 <- data.table(b = 2:10)
+		DT3 <- data.table(b = 2:10, c = 3:11)
 		tablesList <- list(DT1 = DT1, DT2 = DT2, DT3 = DT3)
 	} else {
 		if (inherits(..., "list")){
@@ -12,12 +20,12 @@ listVarsUniques <- function(...){
 		}
 	}
 	countVals <- data.table()
-
 	for (i in 1:length(tablesList)) {
-		# tb <- tablesList[[1]]
 		tb <- tablesList[[i]]
+		# tb <- tablesList[[1]]
 		countVals_int <- data.table()
 		tbVarNames <- names(tb)
+		tbVarNames <- grep(pattern, names(tb), value = TRUE)
 		for (tbVarName in tbVarNames) {
 			# tbVarName <- tbVarNames[1]
 			tbVarLength <- length(unique(tb[, get(tbVarName)]))
@@ -28,27 +36,3 @@ listVarsUniques <- function(...){
 	}
 	return(countVals)
 }
-
-# testListVars <- listVarsUniques(DT1 = DT1, DT2 = DT2, DT3 = DT3)
-#
-#
-# undebug(listVarsUniques)
-# debug(listVarsUniques)
-# DT1 <- data.table(a = 1:10)
-# DT2 <- data.table(b = 2:10)
-# DT3 <- data.table(b = 2:10, c = 3:11)
-
-
-
-# rien a voir : timer.
-# library(R.AlphA)
-# TestTimer <- timer(start = T)
-# for(i in 1:2e4){
-# 	TestTimer <- timer(TestTimer, step = i)
-# }
-# plot(TestTimer$step, TestTimer$dt_seconds, log = "y")
-# ggplot(data = TestTimer, aes(x = step, y = log(dt_seconds)))+ geom_line()# + ylim(c(-8,-2))
-# TestTimer[, log_sec := log(dt_seconds)]
-# TestTimer[, trunc_log_sec := trunc(log_sec)]
-# table(TestTimer$trunc_log_sec)
-# TestTimer[trunc_log_sec >= -4]
