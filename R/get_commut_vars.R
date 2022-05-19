8fafcd4d236c2a35dec5d1893c6f18ee760020f7
AlphAEl3ctr0 <raphael.flambard@outlook.fr>
2022-05-19 16:55:34 +0200

    get_commut_vars - WIP

--- R/get_commut_vars.R
+++ R/get_commut_vars.R
@@ -1,3 +1,4 @@
+# rm(list = ls())
 #fonction de jointure avec la table de commut :  definition de la cle, choix des variables à récupérer--------
 # réfléchir au format que devrait avoir la sortie de cette fonction
 #' ajouter des nombres de commut a une table
@@ -18,6 +19,7 @@ get_commut_vars <- function(
 	, cle_commut = "age"
 	, cle_commut_pop = cle_commut
 	, cle_commut_com = cle_commut
+	, key_pattern = "dim_|inc_"
 ){
 	manualrun <- T
 	manualrun <- F
@@ -27,44 +29,70 @@ get_commut_vars <- function(
 		workRRoot <- stringr::str_extract(root, ".*WorkR")
 		tbls <- readRDS(file.path(workRRoot, "pop stats", "INPUTS", "tables.rds"))
 		library(R.AlphA)
-		commutTable = Complete_commut(tbls$TH)
+		commutTable = Complete_commut(tbls$classic$t_vie)
 		popTable = generate_pop(10)[, age := year(dteVision) - year(dteNais)]
+		setnames(popTable, old = c("sexe", "age"), new = c("dim_sexe", "inc_age"))
+		popTable[ dim_sexe == "H", dim_sexe := "M"]
+		popTable
 		# popTable = get_commut_vars(popTable, tbls$TH, "ax") # tests pour gerer le cas ou on a deja un ax : il faudrait l'ecraser au lieu de buguer
 		commutVars = "ax"
 		cle_commut = "age"
 		cle_commut_pop = cle_commut
 		cle_commut_com = cle_commut
+		key_pattern = "dim_|inc_"
 	}
-	# browser()
-	# cle_commut <- c("age", "sexe") #peut-être stocker la cle dans une variable et la récupérer
-	# popTable[, int_get_commut_key := 1:nrow(popTable)] # non : ca modifie la table passee en argument ce qui est de la merde
-	popTable$int_get_commut_key = 1:nrow(popTable) # ok : ne modifie pas la table passee en argument
-	variables_table_commut <- c(cle_commut_com, commutVars)
-	variables_table_pop <- c(cle_commut_pop, "int_get_commut_key")
-	popTable[, ..variables_table_pop]
+	# 1 - index
+	popTable$popIndex = 1:nrow(popTable) # ok : ne modifie pas la table passee en argument
+
+	popTable
+	commutTable
+	library(rlang) # pour missing_arg() --> a etudier
+
+	# 2 - key
+	commutTable$gc_key <- dimsCol(commutTable, pattern = key_pattern)
+	popTable$gc_key    <- dimsCol(popTable   , pattern = key_pattern)
+
+	# 3 - merge
+	# plutot utiliser mergeLifeTable ?
+	# mergedPopCom <- merge(popTable, commutTable, by  = "gc_key", all.x = TRUE)
+	# anyDuplicated(mergedPopCom$popIndex)
+	variables_table_commut <- c("gc_key", commutVars)
+	variables_table_pop <- c("gc_key", "popIndex")
+	# popTable[, ..variables_table_pop]
 	merged_table <- merge(
 		x = popTable[ , ..variables_table_pop]
 		# x = sPop
 		, y = commutTable[, ..variables_table_commut]
 		# , y = table_commut_finale[, ..variables_table_commut]
-		, by.x = cle_commut_pop
-		, by.y = cle_commut_com
+		# , by.x = cle_commut_pop
+		# , by.y = cle_commut_com
+		, by = "gc_key"
 		, all.x = TRUE # 2021.09.29
 	)
-	res_order_select <- merged_table[order(int_get_commut_key), ..commutVars]
+
+	# 4 - reorder, check, select
+	res_order <- merged_table[order(popIndex)]
+	checkIndex <- all.equal(res_order$popIndex, popTable$popIndex)
+	if (!checkIndex) {
+		stop("index has changed during merge")
+	} else {
+		res_order_select <- merged_table[order(popIndex), ..commutVars]
+	}
+	as.vector(res_order_select)
 
 	if (length(commutVars) >1) {
 		# print(paste0(
 		# 	"length commutVars : ", length(commutVars), " - returning table"
 		# ))
-		return(res_order_select)
+		gcResult <- res_order_select
 	} else {
 		# print(paste0(
 		# 	"length commutVars : ", length(commutVars), " - returning vector"
 		# ))
-		return(res_order_select[, result := res_order_select[, 1]]$result) # assez moche, a corriger si possible (permet de renvoyer un vecteur plutot qu'une data.table)
+		gcResult <- res_order_select[, result := res_order_select[, 1]]$result # assez moche, a corriger si possible (permet de renvoyer un vecteur plutot qu'une data.table)
 	}
 
+	return(gcResult)
 }
 
 # rm(list = ls())
